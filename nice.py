4c8526fcbd5a1b5991c3617b99e06940b560a182
Alexis Schad <schad.alexis@gmail.com>
2023-12-20 09:09:54 +0100

    nice.py

--- nice.py
+++ nice.py
@@ -1,36 +1,17 @@
-from src.nice.api import *
+import sys
+import os
 
-if __name__ == "__main__":
-    item = AdvancedItem(
-        author="schadocalex",
-        name="MyItem",
-        waypoint_type="None",
-        icon_filepath=None,
-        entities=[
-            Mesh(
-                loc=Loc(
-                    (4.860170841217041, -12.169404029846191, 7.0),
-                    (0.652657151222229, -0.013293812982738018, -0.16387097537517548, -0.7396000623703003),
-                ),
-                mesh_filepath=r"C:\Users\schad\Documents\Trackmania\Items\NICE\Part1.Mesh.Gbx",
-                # r"C:\Users\schad\Documents\Trackmania\Items\NICE\Part1.Shape.Gbx",
-            ),
-            Mesh(
-                loc=Loc(
-                    (-5.0, 0, 2.0),
-                    (0.5866422057151794, -0.6842648386955261, -0.4183301031589508, 0.11239420622587204),
-                ),
-                mesh_filepath=r"C:\Users\schad\Documents\Trackmania\Items\NICE\Part2.Mesh.Gbx",
-                # r"C:\Users\schad\Documents\Trackmania\Items\NICE\Part1.Shape.Gbx",
-            ),
-            # Gate(
-            #     loc=Loc((0, 0, 0), (1, 0, 0, 0)),
-            #     shape_filepath=r"C:\Users\schad\Documents\Trackmania\Items\NICE\Part1.Shape.Gbx",
-            #     gameplayId="ReactorBoost2",
-            # ),
-        ],
-    )
+import bpy
+from .blender.importer import TM_OT_NICE_Item_Import, TM_PT_NICE
 
-    item_bytes = item.generate()
-    with open(r"C:\Users\schad\Documents\Trackmania\Items\nice.Item.Gbx", "wb") as f:
-        f.write(item_bytes)
+classes = (TM_OT_NICE_Item_Import, TM_PT_NICE)
+
+
+def NICE_register():
+    for cls in classes:
+        bpy.utils.register_class(cls)
+
+
+def NICE_unregister():
+    for cls in reversed(classes):
+        bpy.utils.unregister_class(cls)
