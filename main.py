ba259509b96491c7fe60ed92b1a1d56db219d84b
Alexis Schad <schad.alexis@gmail.com>
2024-03-14 20:37:57 +0100

    add update_physics script

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+from src.gui import main
+
+# import src.update_physics
+
 if __name__ == "__main__":
-    from src.gui import main
 
     main()
+    # src.update_physics.main()
---
+++ src/update_physics.py
@@ -0,0 +1,72 @@
+import os
+import sys
+from glob import glob
+
+from PySide6.QtWidgets import QApplication, QFileDialog
+
+from .parser import parse_file, generate_file
+from .editor import GbxEditorUi, GbxEditorUiWindow
+from .utils.misc import update_surf
+
+GUI = False
+
+
+def get_all_models(data):
+    if data.classId == 0x2E002000:
+        yield from get_all_models(data.body[0x2E002019].EntityModel)
+    elif data.classId == 0x09145000:
+        for ent in data.body.Ents:
+            if ent.model._index > 0:
+                yield from get_all_models(ent.model)
+    elif data.classId == 0x09159000:
+        yield data
+    else:
+        print("Unknown classId: " + str(data.classId))
+
+
+def update_surf(surf, from_physics_id, to_physics_id):
+    for materialId in surf.body[0x0900C003].materialsIds:
+        if materialId.physicsId == from_physics_id:
+            materialId.physicsId = to_physics_id
+
+    for tri in surf.body[0x0900C003].surf.data.triangles:
+        if tri.materialId.physicsId == from_physics_id:
+            tri.materialId.physicsId = to_physics_id
+
+
+def main():
+    if GUI:
+        app = QApplication.instance() or QApplication(sys.argv)
+        win = GbxEditorUiWindow()
+        win.setWindowTitle("Before")
+        win2 = GbxEditorUiWindow()
+        win2.setWindowTitle("After")
+
+    # loop over all items
+    for file in glob(r"./items/*.Item.Gbx", recursive=True):
+        print(file)
+        data = parse_file(file)
+
+        if GUI:
+            win.set_data(data)
+
+        for model in get_all_models(data):
+            if model.body.isMeshCollidable:
+                # here we manage collidables meshes
+                for custom_mat in model.body.Mesh.body[0x90BB000].customMaterials:
+                    mat = custom_mat.materialUserInst.body[0x90FD000]
+                    if mat.surfacePhysicId == "TechSuperMagnetic":  # and mat.materialName == "m0"
+                        mat.surfacePhysicId = "TechMagneticAccel"
+
+            elif model.body.Shape._index > 0:
+                # here we manage collidables shapes
+                update_surf(model.body.Shape, "Ice", "RoadIce")
+
+        # Generate the new file and overwrite it
+        with open(file, "wb") as f:
+            f.write(generate_file(data))
+
+        if GUI:
+            win2.set_data(data)
+            app.exec()
+            return  # only show GUI for the first item
