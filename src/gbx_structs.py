d0e9cfd48d8808a922326a50b0868ea0086dae7a
ar <AR.Online.Persona@proton.me>
2026-03-18 17:22:43 +0100

    Fix TM2020 collection ids and preserve raw chunk 0x03043040

--- src/gbx_structs.py
+++ src/gbx_structs.py
@@ -418,18 +418,49 @@ def encode_lookbackstring(obj, ctx):
     return Container(version=3, index=idx, string=obj)
 
 
+def encode_collection_lookbackstring(obj, ctx):
+    gbx_data = ctx._root._params.gbx_data
+    idx = 0x40000000
+    text = str(obj)
+
+    if text == "Unassigned":
+        idx = 0xBFFFFFFF
+    elif text == "":
+        idx = 0xFFFFFFFF
+    elif text in GbxCollectionIdsFromStr:
+        idx = GbxCollectionIdsFromStr[text]
+    elif len(text) > 1 and text[0] == "U" and text[1:].isdigit():
+        idx = int(text[1:])
+    elif text in gbx_data["lookbackstring_table"]:
+        idx = 0x40000000 | gbx_data["lookbackstring_table"][text]
+    else:
+        gbx_data["lookbackstring_index"] += 1
+        gbx_data["lookbackstring_table"][text] = gbx_data["lookbackstring_index"]
+        idx = 0x40000000
+
+    return Container(version=3, index=idx, string=text)
+
+
 class TGbxLookbackString(str):
     pass
 
 
+_GbxLookbackStringStruct = Struct(
+    "version" / If(need_version, ExprValidator(Int32ul, obj_ == 3)),
+    "index" / Int32ul,
+    "string" / If(need_string, GbxString),
+)
+
 GbxLookbackString = ExprAdapter(
-    Struct(
-        "version" / If(need_version, ExprValidator(Int32ul, obj_ == 3)),
-        "index" / Int32ul,
-        "string" / If(need_string, GbxString),
-    ),
+    _GbxLookbackStringStruct,
     lambda *args: TGbxLookbackString(decode_lookbackstring(*args)),
     encode_lookbackstring,
+)
+
+GbxCollectionLookbackString = ExprAdapter(
+    _GbxLookbackStringStruct,
+    lambda *args: TGbxLookbackString(decode_lookbackstring(*args)),
+    encode_collection_lookbackstring,
 )
 
 
@@ -470,7 +501,7 @@ class GbxLookbackStringContext(Construct):
 
 GbxMeta = Struct(
     "id" / GbxLookbackString,
-    "collection" / GbxLookbackString,
+    "collection" / GbxCollectionLookbackString,
     "author" / GbxLookbackString,
 )
 
@@ -1064,27 +1095,7 @@ body_chunks[0x03043029] = Struct(
 body_chunks[0x0304302A] = Struct(
     "u01" / GbxBool,
 )
-body_chunks[0x03043040] = GbxLookbackStringContext(
-    Struct(
-        "version" / Int32ul,  # 8
-        "u01" / Int32sl,
-        "size" / Int32sl,
-        "_listVersion" / Int32ul,
-        "anchoredObjects" / GbxArrayOf(GbxClass),
-        "itemsOnItem"
-        / If(
-            lambda this: this.version >= 1 and this.version != 5 and this.version < 8,
-            GbxArrayOf(GbxInt2),
-        ),
-        StopIf(this.version < 5),
-        "blockIndexes" / GbxArrayOf(Int32sl),
-        "snapItemGroups" / If(this.version < 7, GbxArrayOf(Int32sl)),
-        "itemIndexes" / If(this.version >= 6, GbxArrayOf(Int32sl)),
-        "snapItemGroups" / If(this.version >= 7, GbxArrayOf(Int32sl)),
-        "u07" / If(this.version != 6, GbxArrayOf(Int32sl)),
-        "snappedIndexes" / GbxArrayOf(Int32sl),
-    )
-)
+body_chunks[0x03043040] = Struct("_unknownChunkId" / GreedyBytes)
 body_chunks[0x03043048] = Struct(
     "version" / Int32ul,
     "listBlocksVersion" / Int32ul,
